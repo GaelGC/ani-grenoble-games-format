7fc6b5baec1a323f184f848edc966b3d01d17939
Gael GC <83537366+GaelGC@users.noreply.github.com>
2021-06-05 17:58:19 +0100

    [TYPES] Add the parser

--- src/QuestionTypes.ts
+++ src/QuestionTypes.ts
@@ -1,21 +1,42 @@
-interface QuestionBase<TypeName> {
+import Ajv from "ajv";
+import Result = require("ts-result");
+import fs = require('fs');
+
+export interface QuestionBase<TypeName> {
     type: TypeName;
     name: string;
     points: number;
     hints: string[];
 };
 
-interface BlindTestQuestion extends QuestionBase<"BlindTestQuestion"> {
+export interface BlindTestQuestion extends QuestionBase<"BlindTestQuestion"> {
     path: string;
 };
 
-interface TextQuestion extends QuestionBase<"TextQuestion"> {
+export interface TextQuestion extends QuestionBase<"TextQuestion"> {
     question: string;
     answer: string;
 };
 
-type Question = BlindTestQuestion | TextQuestion;
+export type Question = BlindTestQuestion | TextQuestion;
 
-interface QuestionSet {
+export interface QuestionSet {
     questions: Question[];
 };
+
+const schema: string = fs.readFileSync(`${__dirname}/schema.json`).toString();
+
+export function parse(json: string): Result<QuestionSet, Error> {
+    const ajv = new Ajv();
+    var val: unknown;
+    try {
+        val = JSON.parse(json);
+    } catch (e) {
+        return Result.err(e);
+    }
+    if (ajv.validate(schema, val)) {
+        return Result.ok(val as QuestionSet);
+    } else {
+        return Result.err(new Error(ajv.errors!.join('\n')));
+    }
+}
